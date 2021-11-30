d73db16e3853d041586c5c83bf35bfde02cfa93a
Andreas Karis <ak.karis@gmail.com>
2021-11-30 21:50:18 +0100

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 # https://doc.dpdk.org/guides/linux_gsg/linux_eal_parameters.html
-LCORES=1,2,3
-MEMORY_CHANNELS=4
+#CPUS=$(cat /proc/self/status | awk '/Cpus_allowed_list/ {print $NF}')
+#LCORES=1,2,3
+#MEMORY_CHANNELS=4
 # https://doc.dpdk.org/guides/sample_app_ug/kernel_nic_interface.html
-PROMISCUOUS="-P"
-HEXMASK="0x3"
-PORTCONFIG="(0,1,2,3)"
+#PROMISCUOUS="-P"
+#HEXMASK="0x1"
+#PORTCONFIG="(0,1,2,3)"
 
-/dpdk-kni -l $LCORES  -n $MEMORY_CHANNELS -- $PROMISCUOUS -p $HEXMASK -m --config="$PORTCONFIG"
+# dpdk-21.11/build/examples/dpdk-l2fwd --vdev=net_tap1 -l 10,12,14,16 -n 4 -- -q 1 -p f --portmap="(0,1)"
+# sh-5.1# dpdk-21.11/build/app/dpdk-testpmd -l 10,12,14,16 -n 4  -a $PCIDEVICE_OPENSHIFT_IO_ENP5S0F0VFIOPCI -- -i --port-topology=chained
+# 
+
+# CPUS=$(cat /proc/self/status | awk '/Cpus_allowed_list/ {print $NF}') dpdk-21.11/build/app/dpdk-testpmd --log-level=10 --legacy-mem --vdev=net_tap1 -l $CPUS -n 4  -a $PCIDEVICE_OPENSHIFT_IO_ENP5S0F0VFIOPCI -- --nb-cores=1 --nb-ports=2 --auto-start --total-num-mbufs=2048 --forward-mode=rxonly
+
+# /dpdk-kni -l $LCORES  -n $MEMORY_CHANNELS -- $PROMISCUOUS -p $HEXMASK -m --config="$PORTCONFIG"
 sleep infinity
