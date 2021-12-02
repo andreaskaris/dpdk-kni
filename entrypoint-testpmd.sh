ce2789a003ab551293cc8fd4e1ff45cad8f178b2
Andreas Karis <ak.karis@gmail.com>
2021-12-02 09:13:49 +0100

    change lcore list , get first 4 lcores, and make lcores and mac configurable

--- entrypoint-testpmd.sh
+++ entrypoint-testpmd.sh
@@ -16,20 +16,60 @@ function addip {
 	fi
 }
 
+function get_first_cpus_from_mask() {
+        local mask=$1
+        local n=$2
+        local hexmask="0x$mask"
+        local pos=0
+        local shifted=0
+        local cpulist=""
+        local numcpus=0
+
+        while [[ $shifted -le $hexmask ]]; do
+                shifted=$((1<<pos))
+                if [[ $(( hexmask & shifted )) -gt 0 ]]; then
+                        if [ "$cpulist" == "" ]; then
+                                cpulist="$pos"
+                        else
+                                cpulist="$cpulist,$pos"
+                        fi
+                        numcpus=$(( numcpus + 1))
+                        if [[ $numcpus -ge $n ]]; then
+                                break
+                        fi
+                fi
+                pos=$(( pos + 1 ))
+        done
+
+        echo $cpulist
+}
+
+
 echo "Get PCI_DEVICE_ID from filter expression"
 PCI_DEVICE_FILTER=${PCI_DEVICE_FILTER:-PCIDEVICE_OPENSHIFT_IO}
 PCI_DEVICE_ID=$(env | egrep "^$PCI_DEVICE_FILTER" | awk -F '=' '{print $NF}' | head -1) 
 
-echo "Get MAC ADDRESS"
-MACADDR=$(echo "macaddr 0" | /dpdk-ethtool -a $PCI_DEVICE_ID | awk '/Port 0 MAC Address/ {print $NF}')
-echo "Get pinned CPUs"
-CPUS=$(cat /proc/self/status | awk '/Cpus_allowed_list/ {print $NF}')
+if [ "$MACADDR" == "" ]; then
+	echo "Get MAC ADDRESS"
+	MACADDR=$(echo "macaddr 0" | /dpdk-ethtool -a $PCI_DEVICE_ID | awk '/Port 0 MAC Address/ {print $NF}')
+else
+	echo "Using MAC ADDRESS '$MACADDR' from configuration"
+fi
+
+if [ "$PINNED_LCORES" == "" ]; then
+	echo "Get the first 4 available CPUs from the Cpus_allowed: list"
+	cpus_allowed_mask=$(cat /proc/self/status | awk '/Cpus_allowed:/ {print $NF}')
+	PINNED_LCORES=$(get_first_cpus_from_mask $cpus_allowed_mask 4)
+	echo "Pinned lcores will be: $PINNED_LCORES"
+else
+	echo "Using PINNED_LCORES '$PINNED_LCORES' from configuration"
+fi
 
 echo "Run addip script in background"
 addip &
 
 echo "Run testpmd and forward everything between dp0 tunnel interface and vfio interface"
-( echo 'start' ; while true ; do echo 'show port stats all' ; sleep 60 ; done ) | /dpdk-testpmd --log-level=10 --legacy-mem --vdev=net_tap1,iface=dp0,mac=${MACADDR} -l $CPUS -n 4 -a $PCI_DEVICE_ID -- --nb-cores=1 --nb-ports=2  --total-num-mbufs=2048 -i
+( echo 'start' ; while true ; do echo 'show port stats all' ; sleep 60 ; done ) | /dpdk-testpmd --log-level=10 --legacy-mem --vdev=net_tap1,iface=dp0,mac=${MACADDR} -l $PINNED_LCORES -n 4 -a $PCI_DEVICE_ID -- --nb-cores=1 --nb-ports=2  --total-num-mbufs=2048 -i
 
 echo "FAILURE! If we got here, this means that it's time for troubleshooting. Testpmd did not run or crashed!"
 sleep infinity
